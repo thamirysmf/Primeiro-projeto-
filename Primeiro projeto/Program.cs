d7074bf832887db0ef95766c83f33d95b9d7062a
Thata <tmirys@hotmail.com>
2022-01-07 13:08:50 -0300

    Adicionar arquivos de projeto.

---
+++ Primeiro projeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro_projeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = "Hello";
+            var b = " World";
+            Console.WriteLine(a + b);
+        }
+    }
+}
